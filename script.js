46c058d2fd51a4b4d6862833e4dd22a27f9f4cf3
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-11-20 16:00:53 +0530

    event handling using jquery

--- script.js
+++ script.js
@@ -9,21 +9,48 @@ $("p").css({
 	fontSize: "30px",
 });
 
-//class
+// //class
 
 $("p").addClass("pre");
 $("p").removeClass("pre");
 $("p").toggleClass("pre");
 
-//check if class if present
+// //check if class if present
 
-if ($("p").hasClass("pre")) {
-	$("p").text("changed");
-}
+// if ($("p").hasClass("pre")) {
+// 	$("p").text("changed");
+// }
 
 $("div").css({
 	height: "100px",
 	width: "100px",
 	"background-color": "cyan",
 	backgroundColor: "red",
+});
+
+//EVENT HANDLING USING JQUERY
+
+// $("div").click(function () {
+// 	alert("Div Clicked");
+// });
+
+// $("div").on("click", function () {
+// 	alert("Div Clicked");
+// });
+
+//it will execute handler function when dom is ready
+// $(document).ready(function () {
+// 	alert("ready !!!");
+// });
+
+//when we want to stop the element functionality
+$("a").on("click", function (event) {
+	event.preventDefault();
+	alert("Anchor tag Clicked");
+});
+
+$("div").on("click", function () {
+	// this -> jo elemet pe click kiya
+	var element = $(this);
+	element.width(element.width() + 10 + "px");
 });
