90382f1323ae9fafb6305ae559a749fd70b22fc0
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-11-23 08:57:33 +0530

    put balls in the basket completed

--- Put Balls in Basket/script.js
+++ Put Balls in Basket/script.js
@@ -17,10 +17,44 @@ let color = [
 ];
 
 let totalBall = 0;
+let lineBall = 0;
 let bottomD = 0;
+let left = 0;
+let height = 6;
+
 let leftD = $(".ball").width();
 
 $(".add-ball").click(function (e) {
-	let index = Math.floor(Math.random() * color.length);
-	console.log(leftD);
+	appendBall();
 });
+
+function appendBall() {
+	if (totalBall == 15) {
+		bottomD += 5;
+		totalBall = 0;
+		left = 0;
+		height += 6;
+		changeBasket();
+	}
+	console.log(totalBall);
+	let index = Math.floor(Math.random() * color.length);
+
+	let newE = $("<div></div>");
+	newE.addClass("ball");
+	newE.css({
+		backgroundColor: color[index],
+		bottom: bottomD + "rem",
+		left: left + "rem",
+	});
+	left += 5;
+	totalBall++;
+	$(".basket").append(newE);
+}
+
+function changeBasket() {
+	console.log("change");
+	$(".basket").css({
+		height: height + "rem",
+		backgroundColor: "rgb(185, 236, 236)",
+	});
+}
