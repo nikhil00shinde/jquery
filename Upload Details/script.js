7cfb80ee80820a2ce88d8c73812c3d2e0dc0d8e9
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-11-26 13:35:00 +0530

    upload details

--- Upload Details/script.js
+++ Upload Details/script.js
@@ -1,9 +1,23 @@
+let white = true;
+let count = 1;
 $("button").on("click", function (e) {
 	let first = $("input").get(0).value;
 	let second = $("input").get(1).value;
 	let third = $("input").get(2).value;
 
 	if (first && second && third) {
+		let item = `<div class="each-details ${count}">
+		Roll no - <span class="highlight">${first}</span> ,
+		<span class="highlight">${second}</span> has scored
+		<span class="highlight">${third}</span> marks
+	</div>`;
+		$(".show-details").append(item);
+		white
+			? $(".each-details" + "." + count).css("backgroundColor", "white")
+			: $(".each-details" + "." + count).css("backgroundColor", "gray");
+
+		white = !white;
+		count++;
 	} else {
 		if (!first) {
 			alert("Please enter Roll Number");
